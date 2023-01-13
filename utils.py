adc52b29ef98921f54bc7e595069f65b00fd6290
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-01-13 09:30:45 +0300

    refactor: Relocate the form validators is_int() and value_exceeds_max() to the utils.py file

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import sys
-
 import pyfiglet
 
 
@@ -7,3 +5,15 @@ def format_time(t):
     minutes = " ".join(list("{:02d}".format(t.minutes)))
     seconds = " ".join(list("{:02d}".format(t.seconds)))
     return pyfiglet.figlet_format("{} : {}".format(minutes, seconds))
+
+
+def value_exceeds_max(value, max_value) -> bool:
+    return int(value) > max_value
+
+
+def is_int(value):
+    try:
+        int(value)
+    except ValueError:
+        return False
+    return True
